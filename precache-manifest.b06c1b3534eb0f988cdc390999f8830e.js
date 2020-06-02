cb77404d529dd36c33c083084c9f90c9ba4c2dc6
jjohnsundar <jjohnsundar9943@gmail.com>
2020-06-02 07:19:31 +0530

    Updates

---
+++ precache-manifest.b06c1b3534eb0f988cdc390999f8830e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072c1034d7a274acd20c1e23bc158411",
+    "url": "/Formvalidation/index.html"
+  },
+  {
+    "revision": "7a57b8ec94004698bd0f",
+    "url": "/Formvalidation/static/css/main.0479fb31.chunk.css"
+  },
+  {
+    "revision": "b6d93d770242c6736c45",
+    "url": "/Formvalidation/static/js/2.969344d3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Formvalidation/static/js/2.969344d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a57b8ec94004698bd0f",
+    "url": "/Formvalidation/static/js/main.7379eec4.chunk.js"
+  },
+  {
+    "revision": "8a5ff45fb1f82a662b55",
+    "url": "/Formvalidation/static/js/runtime-main.836c19fb.js"
+  }
+]);
